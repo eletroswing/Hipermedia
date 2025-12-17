fd50a6cbd9260ec816b6320f13c5c95db6a41101
eletroswing <ytalosilvabatalha@gmail.com>
2025-12-17 19:51:08 -0300

    chore(s3): removing storage

--- packages/api/src/modules/stream.ts
+++ packages/api/src/modules/stream.ts
@@ -1,5 +1,4 @@
 import { redis } from "./../integration/redis";
-import { findRecordedFiles } from "./../integration/s3";
 
 export async function getAllStreams() {
   const allStreams = await redis.keys("hipermidia:*:stream:*")
@@ -9,8 +8,4 @@ export async function getAllStreams() {
 export async function getOneStream(key: string) {
   const allStreams = await redis.get(key)
   return allStreams
-}
-
-export async function getAllRecordings() {
-  return await findRecordedFiles()
 }
